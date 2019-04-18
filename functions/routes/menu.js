1933b6113074d671e0a7dc64b27664a0129a6d53
codeartsoftdev <codeartsoftdev@gmail.com>
2019-04-17 19:36:34 -0500

    menu.js, altamenu.hbs, main.hbs

--- functions/routes/menu.js
+++ functions/routes/menu.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const db = require('firebase-admin').firestore();
 
 router.get('/menu', (req,res) => {
     res.render('Menu/AltaMenu');
 });
+
+
+router.post('/menu', (req,res) => {
+   const {Titulo, Precio, Descripcion, AgregarImagen} = req.body;
+
+   db.collection("Menu").add({
+    Titulo: Titulo,
+    Precio: Precio,
+    Descripcion: Descripcion,
+    Imagen: AgregarImagen
+})
+.then(function(docRef) {
+    // res.send(docRef);
+    var msg = {success: true, title: Titulo};
+    // $("#Msg").val('ok');
+     res.render('Menu/AltaMenu', {msg});
+
+
+})
+.catch(function(error) {
+    // console.error("Error adding document: ", error);
+    res.send(error);
+});
+
+
+});
+
 
 module.exports = router;
