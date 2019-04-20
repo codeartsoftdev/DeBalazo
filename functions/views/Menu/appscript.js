21048ed8f2300805eaffceba60e5ee3247fac1d6
codeartsoftdev <codeartsoftdev@gmail.com>
2019-04-19 23:32:11 -0500

    algunas mejoras

---
+++ functions/views/Menu/appscript.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+        });
+    
+        alert("hola");
+
+    if({{msg.success}})
+    {
+    
+    
+        swal({
+            title: '{{msg.title}}',
+            //text: 'Fue dado de alta exitosamente',
+            text:'Agregado correctamente',
+            showConfirmButton: false,
+            timer: 2600,
+            html: true
+        }) // --> Fin SWAL
+    }
+            
+        
